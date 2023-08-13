a611cb69ba9674c4ced40afc01ecdb1f79054412
Mike Castro <mkcastro24@gmail.com>
2023-08-13 15:29:23 +0800

    add test for another location

--- tests/Feature/IndexLocationFeatureTest.php
+++ tests/Feature/IndexLocationFeatureTest.php
@@ -14,7 +14,7 @@ class IndexLocationFeatureTest extends TestCase
     /**
      * @test
      */
-    public function get_list_of_locations()
+    public function get_toyota_taunton()
     {
         // given
         $parameters = [
@@ -49,6 +49,47 @@ class IndexLocationFeatureTest extends TestCase
             'name' => 'Toyota Taunton',
             'latitude' => '51.475603934275675',
             'longitude' => '-2.3807167145198114',
+        ]);
+    }
+
+    /**
+     * @test
+     */
+    public function get_museum_inn()
+    {
+        // given
+        $parameters = [
+            'latitude' => 51.603983853765925,
+            'longitude' => -1.966490826031952,
+            'radius' => 1,
+            'unit' => 'km',
+        ];
+
+        // when
+        $response = $this->get(route('locations.index', $parameters));
+
+        // then
+        $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'name',
+                    'latitude',
+                    'longitude',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+        ]);
+
+        $response->assertJsonCount(1, 'data');
+
+        $response->assertJsonFragment([
+            'name' => 'Museum Inn',
+            'latitude' => '51.603983853765925',
+            'longitude' => '-1.966490826031952',
         ]);
     }
 }
