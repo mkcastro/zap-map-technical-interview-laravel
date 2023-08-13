b955180be095f6e1b5296f05eead1b24ea7d1cf9
Mike Castro <mkcastro24@gmail.com>
2023-08-13 15:16:21 +0800

    use locations.csv to seed locations

--- database/seeders/LocationSeeder.php
+++ database/seeders/LocationSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Location;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage;
 
 class LocationSeeder extends Seeder
 {
@@ -11,6 +13,26 @@ class LocationSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $csv = Storage::get('locations.csv');
+
+        $rows = array_map('str_getcsv', explode("\n", $csv));
+
+        $data = [];
+
+        foreach ($rows as $row) {
+            if (count($row) !== 3) {
+                continue;
+            }
+
+            $data[] = [
+                'name' => $row[0],
+                'latitude' => $row[1],
+                'longitude' => $row[2],
+            ];
+        }
+
+        if (! empty($data)) {
+            Location::insert($data);
+        }
     }
 }
