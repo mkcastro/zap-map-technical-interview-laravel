5c714c901f4c0a72760cba6205a4eba66e88afb3
Mike Castro <mkcastro24@gmail.com>
2023-08-13 15:44:20 +0800

    create concretion for km unit

---
+++ app/Concretions/KilometerLocationConcretion.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Concretions;
+
+use App\Interfaces\IndexLocationInterface;
+use App\Models\Location;
+
+class KilometerLocationConcretion implements IndexLocationInterface
+{
+    public function getLocations($latitude, $longitude, $radius)
+    {
+        $locations = Location::whereRaw(
+            'ST_Distance_Sphere(point(longitude, latitude), point(?, ?)) <= ?',
+            [
+                $longitude,
+                $latitude,
+                $radius * 1000,
+            ]
+        )
+            ->get();
+
+        return $locations;
+    }
+}
