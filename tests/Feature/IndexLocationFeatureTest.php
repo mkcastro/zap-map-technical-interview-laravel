a771c593ee5b35e16d57184bf717b88bfd853904
Mike Castro <mkcastro24@gmail.com>
2023-08-13 15:06:37 +0800

    create get list of locations

--- tests/Feature/IndexLocationFeatureTest.php
+++ tests/Feature/IndexLocationFeatureTest.php
@@ -9,12 +9,43 @@ use Tests\TestCase;
 class IndexLocationFeatureTest extends TestCase
 {
     /**
-     * A basic feature test example.
+     * @test
      */
-    public function test_example(): void
+    public function get_list_of_locations()
     {
-        $response = $this->get('/');
+        // given
+        $parameters = [
+            'latitude' => 51.475603934275675,
+            'longitude' => -2.3807167145198114,
+            'radius' => 1,
+            'unit' => 'km',
+        ];
 
+        // when
+        $response = $this->get(route('locations.index', $parameters));
+
+        // then
         $response->assertStatus(200);
+
+        $response->assertJsonStructure([
+            'data' => [
+                '*' => [
+                    'id',
+                    'name',
+                    'latitude',
+                    'longitude',
+                    'created_at',
+                    'updated_at',
+                ],
+            ],
+        ]);
+
+        $response->assertJsonCount(1, 'data');
+
+        $response->assertJsonFragment([
+            'name' => 'Toyota Taunton',
+            'latitude' => '51.475603934275675',
+            'longitude' => '-2.3807167145198114',
+        ]);
     }
 }
